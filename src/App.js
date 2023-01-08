4a531b92de9d62dc4e26f561a1df876f2a3e27ab
Soyeon Park <psy_sott120@naver.com>
2023-01-08 20:46:39 +0900

    머지 충돌 테스트

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Sott from "./Sott";
 import Sungtt from "./sungtt";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Sungtt data={"프롭스좀 보내겠습니다."} />
-      <Sungtt data={"프롭스좀 보내겠습니다."} />
-      <Sott />
-      <p>sott branch에서 작성한 내용입니다.</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Sungtt data={"프롭스좀 보내겠습니다."} />
+            <Sungtt data={"프롭스좀 보내겠습니다."} />
+            <Sott />
+            <p>sott branch에서 작성한 내용입니다.</p>
+            <p>sott branch에서 작성한 두 번쨰 내용입니다.</p>
+        </div>
+    );
 }
 
 export default App;
