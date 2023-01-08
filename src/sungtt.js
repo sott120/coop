4ddedcc51fc6e3abc6f5d40faaa7a1c32ac4625c
qirtudgus <qirtudgus@naver.com>
2023-01-08 20:11:33 +0900

    성트 : 성트 브랜치의 2번 커밋
    sungtt에 props 전달

--- src/sungtt.js
+++ src/sungtt.js
@@ -1,6 +1,7 @@
-export default function Sungtt() {
+export default function Sungtt(props) {
   return (
     <>
+      <p>{props.data}</p>
       <p>성트가 만든 컴포넌트</p>
       <p>성트의 첫 브랜치, 작업물이 겹치지않음</p>
     </>
